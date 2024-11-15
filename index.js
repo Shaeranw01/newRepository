a7534c30c79f0211c81e6149b1129741c5ac7d37
Sharanjeet Kaur <sharanjeetkaur@Sharanjeets-MacBook-Pro.local>
2024-11-15 14:34:24 -0800

    new branch added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("hello from vs code");
 
-<button>lets try commit again</button>
+<button>lets try commit again</button>;
 <button>lets add another button</button>
+console.log("code from a new branch");
 
 
