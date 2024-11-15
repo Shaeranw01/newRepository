a6d321fa72f5651410f76fb08effbc1cf47be9b0
Sharanjeet Kaur <sharanjeetkaur@Sharanjeets-MacBook-Pro.local>
2024-11-15 14:49:43 -0800

    add more code to branch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("hello from vs code");
 <button>lets try commit again</button>;
 <button>lets add another button</button>;
 console.log("lets add a branch");
+console.log("lets add more code");
 
 
 
